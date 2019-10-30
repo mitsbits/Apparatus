70faf335cd256eca8b363269b360dc440db0c6e6
mitsbits <mitsbits@gmail.com>
2019-10-30 21:52:09 +0200

    .

---
+++ Borg/Framework/Borg.Framework.MVC/ModuleUiPostConfigure.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.StaticFiles;
+using Borg.Infrastructure.Core;
+using Microsoft.Extensions.FileProviders;
+
+namespace Borg.Framework.MVC
+{
+    public abstract class ModuleUiPostConfigure : IPostConfigureOptions<StaticFileOptions>
+    {
+
+        private static readonly string basePath = "wwwroot";
+
+        public ModuleUiPostConfigure(IHostingEnvironment environment)
+        {
+            Environment = environment;
+        }
+
+        public IHostingEnvironment Environment { get; }
+
+        public void PostConfigure(string name, StaticFileOptions options)
+        {
+            options = Preconditions.NotNull(options, nameof(options));
+
+            // Basic initialization in case the options weren't initialized by any other component
+            options.ContentTypeProvider = options.ContentTypeProvider ?? new FileExtensionContentTypeProvider();
+            if (options.FileProvider == null && Environment.WebRootFileProvider == null)
+            {
+                throw new InvalidOperationException("Missing FileProvider.");
+            }
+
+            options.FileProvider = options.FileProvider ?? Environment.WebRootFileProvider;
+
+            var filesProvider = new ManifestEmbeddedFileProvider(GetType().Assembly, basePath);
+            options.FileProvider = new CompositeFileProvider(options.FileProvider, filesProvider);
+        }
+    }
+}

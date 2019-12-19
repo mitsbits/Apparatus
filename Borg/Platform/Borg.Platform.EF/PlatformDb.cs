aedb9d6222f5802d93e4f77efeeddf413fb6662c
mitsbits <mitsbits@gmail.com>
2019-12-19 22:51:13 +0200

    .

--- Borg/Platform/Borg.Platform.EF/PlatformDb.cs
+++ Borg/Platform/Borg.Platform.EF/PlatformDb.cs
@@ -1,5 +1,7 @@
 ﻿using Borg.Framework.EF;
 using Borg.Framework.EF.System.Domain.Silos;
+using Borg.Framework.EF.System.Domain.System;
+using Borg.Platform.EF.SystemEntites;
 using Microsoft.EntityFrameworkCore;
 
 namespace Borg.Platform.EF
@@ -8,5 +10,8 @@ namespace Borg.Platform.EF
     {
         public DbSet<Language> Languages { get; set; }
         public DbSet<Tenant> Tenants { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<Page> Pages { get; set; }
+        public DbSet<Folder> Dictionaries { get; set; }
     }
 }

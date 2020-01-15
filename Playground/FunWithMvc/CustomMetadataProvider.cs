547ffd3f2b51a9ac389cf5e4d4d53ba6fb416bed
mitsbits <mitsbits@gmail.com>
2020-01-15 21:04:15 +0200

    .

---
+++ Playground/FunWithMvc/CustomMetadataProvider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FunWithMvc
+{
+    public class CustomMetadataProvider : IDisplayMetadataProvider
+    {
+        public void CreateDisplayMetadata(DisplayMetadataProviderContext context)
+        {
+            var propertyAttributes = context.Attributes;
+            var modelMetadata = context.DisplayMetadata;
+            var propertyName = context.Key.Name;
+        }
+    }
+}
--- Playground/FunWithMvc/Startup.cs
+++ Playground/FunWithMvc/Startup.cs
@@ -23,7 +23,7 @@ namespace FunWithMvc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddMvcOptions(m => m.ModelMetadataDetailsProviders.Add(new CustomMetadataProvider())); ;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
